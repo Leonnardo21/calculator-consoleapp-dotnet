ce834ffcb948999877f5951d8d100e4e96411707
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-08-10 10:42:51 -0300

    feat: Implemented division function

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace Calculator
         static void Main(string[] args)
         {
             // Soma();
-            Subtracao();
+            // Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -27,14 +28,29 @@ namespace Calculator
         {
             Console.Clear();
             Console.Write("Digite o primeiro valor: ");
-            float v1 = int.Parse(Console.ReadLine());
+            float v1 = float.Parse(Console.ReadLine());
 
             Console.Write("Digite o segundo valor: ");
-            float v2 = int.Parse(Console.ReadLine());
+            float v2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine();
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração entre {v1} e {v2} é igual a {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão entre {v1} e {v2} é igual a {resultado}");
             Console.ReadKey();
         }
     }
