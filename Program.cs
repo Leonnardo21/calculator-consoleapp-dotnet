968fd3f20818fafd31b708addff4b1f90971d4d9
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-08-10 10:26:24 -0300

    Initial Base Project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma entre {v1} e {v2} é igual a {resultado}");
+        }
+    }
+}
