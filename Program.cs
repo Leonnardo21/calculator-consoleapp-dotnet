055d19b6b5e70436d9a632ab847558beb8475a11
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-08-12 10:19:05 -0300

    feat: Implemented Menu function

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            // Soma();
-            // Subtracao();
-            // Divisao();
-            Multiplicacao();
+            Menu();
         }
 
         static void Soma()
@@ -25,6 +22,7 @@ namespace Calculator
             float resultado = v1 + v2;
             Console.WriteLine($"O resultado da soma entre {v1} e {v2} é igual a {resultado}");
             Console.ReadKey();
+            Menu();
         }
         static void Subtracao()
         {
@@ -39,6 +37,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração entre {v1} e {v2} é igual a {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -54,6 +53,7 @@ namespace Calculator
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão entre {v1} e {v2} é igual a {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -69,7 +69,32 @@ namespace Calculator
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da multiplicação entre {v1} e {v2} é igual a {resultado}");
             Console.ReadKey();
+            Menu();
 
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("--------- Escolha uma operação ---------");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Seleciona uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
         }
     }
 }
